d91f4b12db4e74ad53d2890d3c6b62642818b7c9
DL-WG <72620929+DL-WG@users.noreply.github.com>
2020-10-13 10:44:59 +0100

    Latent GAN

--- Latent-GAN/Velocity Fields/Norm.py
+++
@@ -1,8 +0,0 @@
-# Functions to normalise and denormalise data
-## Inputs can be numpy arrays or tensors
-
-def normalise(x, x_min, x_max):
-    return (x - x_min) / (x_max - x_min)
-
-def denormalise(x_norm, x_min, x_max):
-    return x_norm*(x_max - x_min) + x_min
